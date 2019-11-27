e6ae0eee55f28d3646e6741e1980c43eaa910a67
Vitaly Levitin <VitalyLevitin2@gmail.com>
2019-11-28 00:03:04 +0200

    Added the logic part for question 2.

--- Advanced Java/Maman11_Advanced_Java/Question 2/GameofLife.java
+++ Advanced Java/Maman11_Advanced_Java/Question 2/GameofLife.java
@@ -1,4 +1,68 @@
+import java.security.SecureRandom;
+
 public class GameofLife {
+    private static final int arraySize = 10;
+    private boolean[][] mainMatrix, secondaryMatrix;
+    private static final SecureRandom randomBooleans = new SecureRandom();
+
+    public GameofLife(){
+        mainMatrix = new boolean[arraySize][arraySize];
+        secondaryMatrix = new boolean[arraySize][arraySize];
+        randomize((mainMatrix));
+    }
+
+    public void randomize(boolean[][] matrix){
+        for (int i = 0; i < arraySize; i++) {
+            for (int j = 0; j < arraySize; j++) {
+                matrix[i][j] = randomBooleans.nextBoolean();
+            }
+        }
+    }
+
+    public void nextGeneration(){
+        for (int i = 0; i < arraySize; i++) {
+            for (int j = 0; j < arraySize; j++) {
+                int neighbours = neighbourCount(i,j);
+                lifeOrDeath(i,j,neighbours);
+            }
+        }
 
 
+    }
+
+    private int neighbourCount(int x, int y){
+        int neighbours = 0;
+        if(inbounds(x+1,y)&& mainMatrix[x+1][y])
+            neighbours++;
+        if(inbounds(x-1,y) && mainMatrix[x-1][y])
+            neighbours++;
+        if(inbounds(x,y+1)&& mainMatrix[x][y+1])
+            neighbours++;
+        if(inbounds(x,y-1)&& mainMatrix[x][y-1] )
+            neighbours++;
+        if(inbounds(x+1,y+1)&& mainMatrix[x+1][y+1])
+            neighbours++;
+        if(inbounds(x-1,y+1)&& mainMatrix[x-1][y+1])
+            neighbours++;
+        if(inbounds(x+1,y-1)&& mainMatrix[x+1][y-1])
+            neighbours++;
+        if(inbounds(x-1,y-1)&& mainMatrix[x-1][y-1])
+            neighbours++;
+        return neighbours;
+    }
+
+    private boolean inbounds(int x, int y) {
+        return ((x>0 && x<arraySize-1) && (y>0 && y<arraySize-1));
+    }
+
+    private void lifeOrDeath(int x, int y, int neighbours){
+        if((!mainMatrix[x][y]) && neighbours == 3)
+            secondaryMatrix[x][y] = true;
+        else if(mainMatrix[x][y] && (neighbours == 0 || neighbours == 1 || neighbours > 3))
+            secondaryMatrix[x][y] = false;
+        else if(mainMatrix[x][y] && (neighbours == 2 || neighbours == 3))
+            secondaryMatrix[x][y] = true;
+        else
+            secondaryMatrix[x][y] = false;
+    }
 }
