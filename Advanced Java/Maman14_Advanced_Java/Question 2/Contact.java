44ddec031e43e59038991a0ec9154ddf602674c0
VitalyLevitin <36646281+VitalyLevitin@users.noreply.github.com>
2021-02-21 11:28:54 +0200

    Update Contact.java
    Fixed a git conflict.

--- Advanced Java/Maman14_Advanced_Java/Question 2/Contact.java
+++ Advanced Java/Maman14_Advanced_Java/Question 2/Contact.java
@@ -3,15 +3,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-<<<<<<< HEAD
-public class Contact implements Serializable {
-    private String name;
-    private String number;
-    private final int NUM_LENGTH = 10;
-    public Contact(String name, String number) throws IlegalPhoneNumberException {
-        this.name = name;
-        if(isNumber(number))this.number = number;
-=======
 /**
  * This Class holds the name and number of each
  * contact in the phonebook.
@@ -24,7 +15,6 @@ public class Contact implements Serializable {
     public Contact(String name, String number) throws IlegalPhoneNumberException {
         this.name = name;
         if(isNumber(number) || number.length()==0)this.number = number;
->>>>>>> aad646b56e1b3a40fa4ebaeb8b3c84fde5c6de65
         else throw new IlegalPhoneNumberException("Phone number must contain 10 digits");
     }
 
@@ -36,8 +26,6 @@ public class Contact implements Serializable {
         return this.number;
     }
 
-<<<<<<< HEAD
-=======
     /**
      * This methods checks if the given number
      * is numbers only and following the format
@@ -45,7 +33,6 @@ public class Contact implements Serializable {
      * @param number The number.
      * @return true if it's a number, false else.
      */
->>>>>>> aad646b56e1b3a40fa4ebaeb8b3c84fde5c6de65
     public boolean isNumber(String number){
         try{ Integer.parseInt(number);}
         catch (NumberFormatException e){return false;}
